c3468b42567479601879a672a5a81246c7b16805
akender <arno.kender@gmail.com>
2016-12-12 01:06:24 +0200

    Add files via upload

---
+++ Praktikum5.py
@@ -0,0 +1,176 @@
+__author__ = "Arno Kender (163256IATM)"
+__version__ = "1.0"
+__email__ = "arno.kender@gmail.com"
+
+from PIL import Image
+import numpy as np
+from math import pi, cos, sqrt, log10
+
+# ::: DCT kooder ja dekooder :::
+
+# ::: Parameetrid :::
+blocksize = 8
+
+# ::: Faili lugemine :::
+filename = 'lena512.bmp'
+bmpimg = Image.open(filename, 'r')
+
+
+def getwhole(Q, bs):
+    # Q - quarter matrix
+    # bs - blocksize
+    al, aw = np.shape(Q)
+    B = np.zeros((int(al * 2), int(aw * 2)))  # recovered quarters
+    for M in range(int(al * 2 / bs)):
+        for N in range(int(aw * 2 / bs)):
+            q = np.zeros((bs, bs))
+            x1 = int(M * bs / 2)
+            x2 = int((M + 1) * bs / 2)
+            y1 = int(N * bs / 2)
+            y2 = int((N + 1) * bs / 2)
+            q[0:int(bs / 2), 0:int(bs / 2)] = Q[x1:x2, y1:y2]
+            x1 = int(M * bs)
+            x2 = int((M + 1) * bs)
+            y1 = int(N * bs)
+            y2 = int((N + 1) * bs)
+            B[x1:x2, y1:y2] = q
+    return B
+
+
+def getquarter(B, bs):
+    # B -
+    # bs - blocksize
+    al, aw = np.shape(B)
+    Q = np.zeros((int(al / 2), int(aw / 2)))  # quarters
+    for M in range(int(al / bs)):
+        for N in range(int(aw / bs)):
+            x1 = int(M * bs)
+            x2 = int((M + 1) * bs)
+            y1 = int(N * bs)
+            y2 = int((N + 1) * bs)
+            whole = B[x1:x2, y1:y2]
+            x1 = int(M * bs / 2)
+            x2 = int((M + 1) * bs / 2)
+            y1 = int(N * bs / 2)
+            y2 = int((N + 1) * bs / 2)
+            Q[x1:x2, y1:y2] = whole[0:int(bs / 2), 0:int(bs / 2)]
+    return Q
+
+
+def getalphas(al):
+    # al - array length
+    alphas = [1 / sqrt(al)]
+    for i in range(1, al):
+        alphas.append(sqrt(2 / al))
+    return alphas
+
+
+def getdctsum(Ab, p, q):
+    # Ab - block of A
+    # p, q - pixel index
+    M, N = np.shape(Ab)
+    suml = []  # cos transform list
+    for m in range(M):
+        for n in range(N):
+            suml.append(Ab[m, n] * cos(pi * (2 * m + 1) * p / (2 * M)) * cos(pi * (2 * n + 1) * q / (2 * N)))
+    return sum(suml)
+
+
+def getnorm(Ab):
+    # Ab - block of A
+    al, aw = np.shape(Ab)
+    alphas = getalphas(8)
+    Bb = np.zeros((al, aw))  # block of B
+    for p in range(al):
+        for q in range(aw):
+            Bb[p, q] = alphas[p] * alphas[q] * getdctsum(Ab, p, q)
+    return Bb
+
+
+def getidctsum(Bb, m, n):
+    # Bb - block of B
+    # m, n - pixel index
+    M, N = np.shape(Bb)
+    alphas = getalphas(8)
+    suml = []  # cos transform list
+    for p in range(M):
+        for q in range(N):
+            suml.append(alphas[p] * alphas[q] * Bb[p, q] * cos(pi * (2 * m + 1) * p / (2 * M)) * cos(pi * (2 * n + 1) * q / (2 * N)))
+    return sum(suml)
+
+
+def getrecovered(Bb):
+    # Bb -
+    al, aw = np.shape(Bb)
+    Ab = np.zeros((al, aw))  # block of A
+    for m in range(al):
+        for n in range(aw):
+            Ab[m, n] = getidctsum(Bb, m, n)
+    return Ab
+
+
+def dct2(img, bs):
+    # img - bmp img
+    # bs - blocksize
+    A = np.array(img)  # original matrix
+    al, aw = np.shape(A)  # array length, width
+    B = np.zeros((al, aw))
+    for M in range(int(al / bs)):
+        for N in range(int(aw / bs)):
+            x1 = int(M * bs)
+            x2 = int((M + 1) * bs)
+            y1 = int(N * bs)
+            y2 = int((N + 1) * bs)
+            B[x1:x2, y1:y2] = getnorm(A[x1:x2, y1:y2])
+    return B
+
+
+def idct2(Q, bs):
+    # Q - quarter matrix
+    # bs - blocksize
+    B = getwhole(Q, bs)
+    al, aw = np.shape(B)
+    A = np.zeros((al, aw))  # recovered matrix
+    for M in range(int(al / bs)):
+        for N in range(int(aw / bs)):
+            x1 = int(M * bs)
+            x2 = int((M + 1) * bs)
+            y1 = int(N * bs)
+            y2 = int((N + 1) * bs)
+            A[x1:x2, y1:y2] = getrecovered(B[x1:x2, y1:y2])
+    return A
+
+
+def mse(A, B):
+    al, aw = np.shape(A)
+    li = []
+    for i in range(al):
+        for j in range(aw):
+            li.append(pow((int(B[i, j]) - int(A[i, j])), 2))
+    return sqrt(sum(li) / len(li))
+
+
+def snr(A, B):
+    al, aw = np.shape(A)
+    s1 = 0
+    s2 = 0
+    for i in range(al):
+        for j in range(aw):
+            s1 += int(B[i, j])
+            s2 += int(A[i, j])
+    return 10 * log10(pow(s1, 2) / pow(s1 - s2, 2))
+
+
+bmatrix = dct2(bmpimg, blocksize)
+bquarter = getquarter(bmatrix, blocksize)
+amatrix = idct2(bquarter, blocksize)
+idct = amatrix.astype('uint8')
+im = Image.fromarray(idct)
+im.save('recovered.jpeg')
+
+print('MSE: %s' % mse(amatrix, bmatrix))
+print('SNR: %s' % snr(amatrix, bmatrix))
+print('Tihendamise efektiivsus: %s' % ((8 * 512 * 512) / (8 * 256 * 256)))
+X = 1
+Y = 1
+print('Vabalt valitud bloki teisendatud maatriks:\n%s' % (str(bmatrix[(X - 1) * 8:X * 8, (Y - 1) * 8:Y * 8])))
